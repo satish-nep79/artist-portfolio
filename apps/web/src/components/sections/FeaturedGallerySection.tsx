89119a1d260337c7b143a4e0c14553ebcb4a5c0c
satish-nep <satishnepali79@gmail.com>
2026-07-19 01:33:13 +0300

    wip: Build Featured gallery Section

--- apps/web/src/components/sections/FeaturedGallerySection.tsx
+++ apps/web/src/components/sections/FeaturedGallerySection.tsx
@@ -1,9 +1,27 @@
-import React from 'react'
+import FeaturedArt from "@/components/ui/FeaturedArt";
+import { galleryArtworks } from "@/data/dummy_data/gallery_artworks";
+import Container from "../ui/Container";
 
 const FeaturedGallerySection = () => {
   return (
-    <div id="gallery" className="h-screen text-3xl md:text-5xl lg:text-7xl font-bold text-center pt-44">FeaturedGallerySection</div>
-  )
-}
+    <Container id="gallery">
+      <h2>Gallery</h2>
+      <p className="text-body mb-8 mt-4">
+        A Collection Of Visual Stories created through color, texture and
+        imagination.
+      </p>
+      <div
+        id="gallery"
+        className="columns-1 sm:columns-2 md:columns-2 lg:columns-3 justify-center gap-5"
+      >
+        {galleryArtworks.map((artwork) => (
+          <div key={artwork.id} className="mb-4 break-inside-avoid">
+            <FeaturedArt artwork={artwork} />
+          </div>
+        ))}
+      </div>
+    </Container>
+  );
+};
 
-export default FeaturedGallerySection
+export default FeaturedGallerySection;
--- apps/web/src/data/dummy_data/featured_artworks.ts
+++ apps/web/src/data/dummy_data/featured_artworks.ts
@@ -9,21 +9,22 @@ export const artworks: Artwork[] = [
         "year": 2025,
         "imageUrl": "https://res.cloudinary.com/ioltw4a4/image/upload/v1784216976/gallery-angels_oyd5my.png"
     },
+
     {
         "id": 2,
-        "title": "Whispering Meadows",
-        "category": "Painting",
-        "medium": "Oil on Canvas",
-        "year": 2024,
-        "imageUrl": "https://res.cloudinary.com/ioltw4a4/image/upload/v1784216980/gallery-meadow_ljq4re.png"
-    },
-    {
-        "id": 3,
         "title": "Urban Reflections",
         "category": "Photography",
         "medium": "Digital Print",
         "year": 2023,
         "imageUrl": "https://res.cloudinary.com/ioltw4a4/image/upload/v1784217061/dan-farrell-fT49QnFucQ8-unsplash_1_gtchyi.png"
+    },
+    {
+        "id": 3,
+        "title": "Whispering Meadows",
+        "category": "Painting",
+        "medium": "Oil on Canvas",
+        "year": 2024,
+        "imageUrl": "https://res.cloudinary.com/ioltw4a4/image/upload/v1784216980/gallery-meadow_ljq4re.png"
     },
     {
         "id": 4,
