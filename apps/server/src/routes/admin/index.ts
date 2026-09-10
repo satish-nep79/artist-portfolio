475a28488e15ea899fbe3fde1d7cd114fc712692
satish-nep <satishnepali79@gmail.com>
2026-09-10 19:17:46 +0300

    WIP

--- apps/server/src/routes/admin/index.ts
+++ apps/server/src/routes/admin/index.ts
@@ -10,6 +10,16 @@ const AdminDashboard: FastifyPluginAsync = async (fastify, opts): Promise<void> 
             const html = await PublicHtmlFiles.getHtml(PublicRoutes.DASHBOARD, { cache: false });
             return reply.type('text/html').send(html)
         })
+
+    fastify.get('/login', async function (request, reply) {
+        fastify.log.info(`Serving login HTML for request to ${request.url}`)
+        // const html = await PublicHtmlFiles.getHtml(PublicRoutes.LOGIN, { cache: false })
+        
+        fastify.log.info(`Serving login HTML for request to ${request.url}`)
+
+        return reply.view('auth/login.njk');
+    })
+
 }
 
 export default AdminDashboard
--- apps/server/src/routes/root.ts
+++ apps/server/src/routes/root.ts
@@ -4,10 +4,12 @@ import { PublicHtmlFiles, PublicRoutes } from '../constants/public-routes';
 const RootRoutes: FastifyPluginAsync = async (fastify, opts): Promise<void> => {
 
   fastify.get('/', async function (request, reply) {
+    fastify.log.info(`Redirecting to /admin for request to ${request.url}`);
     return reply.redirect('/admin')
   })
 
   fastify.setNotFoundHandler(async (request, reply) => {
+    fastify.log.info(`Request to ${request.url} not found, serving 404 page`);
     fastify.log.info(`Serving 404 page for request to ${request.url}`);
     const html = await PublicHtmlFiles.getHtml(PublicRoutes.NO_PAGE_FOUND, { cache: true });
     return reply.status(404).type('text/html').send(html);
